29f295a6dd35f2903be337576d06acc6ccb4a553
Pratik <pchy.393@gmail.com>
2021-02-12 19:41:52 +0530

    test server added

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@
 
 sudo apt-get update -y
 sudo apt install build-essential checkinstall -y
-sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev
+sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev -y
 sudo apt-get install python3-pip -y
+pip3 install virtualenv
+virtualenv ./env
+source env/bin/activate
 pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
@@ -16,7 +19,7 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -25,5 +28,5 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
