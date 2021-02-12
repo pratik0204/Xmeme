82fd397c8e3d24f46c4559be1115c1bfec6e45d9
Pratik <pchy.393@gmail.com>
2021-02-12 19:52:45 +0530

    changed yum gets

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 # Any installation related commands
 
 
-sudo yum-get update -y
+sudo yum update -y
 sudo yum install build-essential checkinstall -y
 sudo yum install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev -y
-sudo yum-get install python3-pip -y
+sudo yum install python3-pip -y
 pip3 install virtualenv
 virtualenv ./env
 source env/bin/activate
@@ -14,19 +14,14 @@ python3 manage.py migrate
 
 # Any configuration related commands
 
-sudo yum-get install \
+sudo yum install \
     yum-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo yum-key add -
 
-sudo add-yum-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
 
-sudo yum-get update -y
-sudo yum-get install docker-ce docker-ce-cli containerd.io -y
+sudo yum update -y
+sudo yum install docker-ce docker-ce-cli containerd.io -y
