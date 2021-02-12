3a8ef28736bccc9079970933cca95d622dcd53d6
Pratik <pchy.393@gmail.com>
2021-02-12 19:46:54 +0530

    yum added

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 # Any installation related commands
 
 
-sudo apt-get update -y
-sudo apt install build-essential checkinstall -y
-sudo apt install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev -y
-sudo apt-get install python3-pip -y
+sudo yum-get update -y
+sudo yum install build-essential checkinstall -y
+sudo yum install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev -y
+sudo yum-get install python3-pip -y
 pip3 install virtualenv
 virtualenv ./env
 source env/bin/activate
@@ -14,19 +14,19 @@ python3 manage.py migrate
 
 # Any configuration related commands
 
-sudo apt-get install \
-    apt-transport-https \
+sudo yum-get install \
+    yum-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common -y
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo yum-key add -
 
-sudo add-apt-repository \
+sudo add-yum-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo yum-get update -y
+sudo yum-get install docker-ce docker-ce-cli containerd.io -y
