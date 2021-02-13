0b2259db5ececd6229a941d76e990078596bed2d
Pratik <pchy.393@gmail.com>
2021-02-13 18:33:22 +0530

    giver read write permissions for db

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ source env/bin/activate
 sudo pip3 install -r requirements.txt
 sudo python3 manage.py makemigrations
 sudo python3 manage.py migrate
+chmod 777 db.sqlite3
 
 
 #Additional configuration
